0cdb4a10f8b5e6829b13f6a4dd90dc22b458698f
Pokiwar01 <nightsilverz8@gmail.com>
2024-11-27 16:39:42 +0700

    Add files via upload

---
+++ Part14-Spring MVC and Layers/src/main/java/com/telusko/model/Product.java
@@ -0,0 +1,31 @@
+package com.telusko.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Entity
+public class Product {
+
+    @Id
+    private int prodId;
+    private String prodName;
+    private int price;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "prodId=" + prodId +
+                ", prodName='" + prodName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
